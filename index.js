8d1f7e2151c55586932208f72f52232c6e1611fa
isefroge38 <pavel@virtualbrest.com>
2021-11-23 00:44:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+// comment
+/* comment*/
+
+let myName;
+
+var a;
+a = 7;
+
+var a;
+a = 7;
+var b;
+b = a;
+
+var a = 9;
+
+var myFirstName = "Pavel";
+var myLastName = "Kuharchik";
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+let catName = "Oliver";
+let catSound = "Meow";
+
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+fact = "is awesome!";
+console.log(FCC, fact);
+
+const sum = 10 + 10;
+
+const difference = 45 - 33;
+
+const product = 8 * 10;
+
+const quotient = 66 / 33;
+
+let myVar = 87;
+myVar++;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
