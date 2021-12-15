ca697930241bec035f9b949a3cf0836a17b49047
isefrog38 <isefroge38@gmail.com>
2021-12-16 02:42:22 +0300

    second commit

--- index.js
+++ index.js
@@ -49,10 +49,78 @@ const quotient = 66 / 33;
 let myVar = 87;
 myVar++;
 
+myVar--;
 
+const ourDecimal = 5.7;
+const myDecimal = 5.7;
 
+const product = 2.0 * 2.5;
+const quotient = 4.4 / 2.0;
 
-asd
+const remainder = 11 % 3;
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+const myStr = "FirstLine\n\t\\\SecondLine\nThirdLine";
+
+const myStr = "This is the start." + " This is the end.";
+
+let myStr = 'This is the first sentence.';
+myStr += ' This is the second sentence.';
+
+const myName = "PavelKuh";
+const myStr = "Hello, " + myName + " is freeCodeCamp, how are you?.";
+
+const someAdjective = "awesome";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0];
+
+let myStr = "Jello World";
+myStr = "Hello World";
 
 
 
