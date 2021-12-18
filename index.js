935cf4a1596ed2d18a34cf3419b22085da0815e0
isefrog38 <isefroge38@mail.ru>
2021-12-19 02:34:34 +0300

    19.12.21

--- index.js
+++ index.js
@@ -141,11 +141,29 @@ const myArray = ['One', 1];
 
 const myArray = [['One', 1], ['two', 2]];
 
+const myArray = [50, 60, 70];
+const myData = myArray[0];
+
+const myArray = [18, 64, 99];
+myArray[0] = 45;
+
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+const myData = myArray[2][1];
 
 
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3])
+  
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();
 
-
-
+const myArray = [["John", 23], ["dog", 3]];
+const removedFromMyArray = myArray.shift();
 
 
 
